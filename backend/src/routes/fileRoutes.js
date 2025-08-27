d62390d083e90bf54a19633e2907a2530305e5db
Sam Rodríguez <alguien8191@gmail.com>
2025-08-26 21:31:35 -0500

    Revisión del Frontend y se agrega la funcionalidad de que desde el Backend capture el formulario del Frontend y el archivo, guardandolo localmente

--- backend/src/routes/fileRoutes.js
+++ backend/src/routes/fileRoutes.js
@@ -4,6 +4,23 @@ const upload = require("../middlewares/uploadMiddleware"); // middleware multer
 const { uploadFile } = require("../controllers/fileController");
 
 // Ruta POST /api/files/upload
-router.post("/upload", upload.single("file"), uploadFile);
+router.post("/upload", upload.single("file"), (req, res) => {
+    const coderName = req.body.coderName;
+    const trainingName = req.body.trainingName;
+    console.log("Coder:", coderName, "- Training:", trainingName);
+  
+    if (!req.file) return res.status(400).json({ message: "No se recibió el archivo" });
+
+    res.status(200).json({
+        message: "Archivo subido correctamente",
+        data: {
+            filename: req.file.filename, // nombre guardado en el servidor
+            originalName: req.file.originalname, // nombre que subió el usuario
+            path: req.file.path, // ruta completa en el servidor
+            coderName,
+            trainingName
+        }
+    });
+});
 
 module.exports = router;
