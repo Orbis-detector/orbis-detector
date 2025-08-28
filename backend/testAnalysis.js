3fc3c678981f1ff97fbb61caf388ff1721fc4081
JavierDevPro <gjangsam@gmail.com>
2025-08-28 14:04:13 -0500

    Integracion del archivo de analisis

--- backend/testAnalysis.js
+++ backend/testAnalysis.js
@@ -1,6 +1,6 @@
 import { proccessFileAnalysisAndSave } from './src/services/analysisService.js';
 
-// ⚠️ Cambia estas rutas/IDs a algo real de tu DB
+//  Cambia estas rutas/IDs a algo real de tu DB
 const filePath = './src/uploads/relato_elara.pdf';   // Archivo que quieras analizar
 const dbFileId = 1;                         // ID que exista en tu tabla Files
 
