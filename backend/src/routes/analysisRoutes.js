9a39e6e12030e538261385340fbd71e549403bde
JavierDevPro <gjangsam@gmail.com>
2025-08-28 14:18:08 -0500

    gitignore

--- backend/src/routes/analysisRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { analyzeFile } from "../controllers/analysisController.js";
-
-const router = Router();
-
-// POST /analysis
-router.post("/", analyzeFile);
-
-export default router;
